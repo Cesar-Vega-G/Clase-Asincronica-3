5363708c24786ce2e996a581dc246352057ab3c8
cesar <cesarvega1854@gmial.com>
2025-03-21 23:46:52 -0600

    Cola terminado

--- Clase Asincronica 3/Program.cs
+++ Clase Asincronica 3/Program.cs
@@ -1,2 +1,144 @@
-﻿// See https://aka.ms/new-console-template for more inform
-Console.WriteLine("Hello, World!");
+﻿
+public class ColaSimple
+{
+    private int[] elementos;
+    private int inicio;
+    private int fin;
+    private int capacidad;
+
+    public ColaSimple(int capacidad)
+    {
+        this.capacidad = capacidad;
+        elementos = new int[capacidad];
+        inicio = -1;
+        fin = -1;
+    }
+
+    public void Agregar(int valor)
+    {
+        if (fin == capacidad - 1)
+        {
+            Console.WriteLine("La cola está llena (desbordamiento)");
+        }
+        else
+        {
+            if (inicio == -1)
+            {
+                inicio = 0;
+            }
+            elementos[++fin] = valor;
+        }
+    }
+
+    public int Eliminar()
+    {
+        if (inicio == -1)
+        {
+            Console.WriteLine("La cola está vacía (subdesbordamiento)");
+            return -1;
+        }
+        else
+        {
+            int valor = elementos[inicio];
+            if (inicio == fin)
+            {
+                inicio = -1;
+                fin = -1;
+            }
+            else
+            {
+                inicio++;
+            }
+            return valor;
+        }
+    }
+
+    public int VerPrimero()
+    {
+        if (inicio == -1)
+        {
+            Console.WriteLine("La cola está vacía");
+            return -1;
+        }
+        else
+        {
+            return elementos[inicio];
+        }
+    }
+}
+
+public class ColaCircular
+{
+    private int[] elementos;
+    private int inicio;
+    private int fin;
+    private int capacidad;
+
+    public ColaCircular(int capacidad)
+    {
+        this.capacidad = capacidad;
+        elementos = new int[capacidad];
+        inicio = -1;
+        fin = -1;
+    }
+
+    public void Agregar(int valor)
+    {
+        if ((fin + 1) % capacidad == inicio)
+        {
+            Console.WriteLine("La cola está llena (desbordamiento)");
+        }
+        else
+        {
+            if (inicio == -1)
+            {
+                inicio = 0;
+            }
+            fin = (fin + 1) % capacidad;
+            elementos[fin] = valor;
+        }
+    }
+
+    public int Eliminar()
+    {
+        if (inicio == -1)
+        {
+            Console.WriteLine("La cola está vacía (subdesbordamiento)");
+            return -1;
+        }
+        else
+        {
+            int valor = elementos[inicio];
+            if (inicio == fin)
+            {
+                inicio = -1;
+                fin = -1;
+            }
+            else
+            {
+                inicio = (inicio + 1) % capacidad;
+            }
+            return valor;
+        }
+    }
+
+    public int VerPrimero()
+    {
+        if (inicio == -1)
+        {
+            Console.WriteLine("La cola está vacía");
+            return -1;
+        }
+        else
+        {
+            return elementos[inicio];
+        }
+    }
+}
+
+class Program
+{
+    static void Main() { }
+}
+
+
--- Pruebas Unitarias/TestCola.cs
+++ Pruebas Unitarias/TestCola.cs
@@ -1,11 +1,180 @@
-﻿namespace Pruebas_Unitarias
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class ColaSimpleTests
 {
-    [TestClass]
-    public sealed class TestCola
+    [TestMethod]
+    public void TestAgregarYVerPrimero()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+        var cola = new ColaSimple(3);
+        cola.Agregar(1);
+        cola.Agregar(2);
+
+        Assert.AreEqual(1, cola.VerPrimero());
+    }
+
+    [TestMethod]
+    public void TestEliminar()
+    {
+        var cola = new ColaSimple(3);
+        cola.Agregar(1);
+        cola.Agregar(2);
+
+        int eliminado = cola.Eliminar();
+
+        Assert.AreEqual(1, eliminado);
+        Assert.AreEqual(2, cola.VerPrimero());
+    }
+
+    [TestMethod]
+    public void TestEliminarUltimoElemento()
+    {
+        var cola = new ColaSimple(3);
+        cola.Agregar(1);
+        cola.Agregar(2);
+        cola.Agregar(3);
+
+        cola.Eliminar();
+        cola.Eliminar();
+        int eliminado = cola.Eliminar();
+
+        Assert.AreEqual(3, eliminado);
+        Assert.AreEqual(-1, cola.VerPrimero());
+    }
+
+    [TestMethod]
+    public void TestDesbordamiento()
+    {
+        var cola = new ColaSimple(2);
+        cola.Agregar(1);
+        cola.Agregar(2);
+
+        cola.Agregar(3);
+
+        Assert.AreEqual(1, cola.VerPrimero());
+    }
+
+    [TestMethod]
+    public void TestSubdesbordamiento()
+    {
+        var cola = new ColaSimple(3);
+
+        int eliminado = cola.Eliminar();
+
+        Assert.AreEqual(-1, eliminado);
+    }
+
+    [TestMethod]
+    public void TestVerPrimeroConColaVacia()
+    {
+        var cola = new ColaSimple(3);
+
+        int primero = cola.VerPrimero();
+
+        Assert.AreEqual(-1, primero);
     }
 }
+
+[TestClass]
+public class ColaCircularTests
+{
+    [TestMethod]
+    public void TestAgregarYVerPrimeroCircular()
+    {
+        var cola = new ColaCircular(3);
+        cola.Agregar(1);
+        cola.Agregar(2);
+
+        Assert.AreEqual(1, cola.VerPrimero());
+    }
+
+    [TestMethod]
+    public void TestEliminarCircular()
+    {
+        var cola = new ColaCircular(3);
+        cola.Agregar(1);
+        cola.Agregar(2);
+
+        int eliminado = cola.Eliminar();
+
+        Assert.AreEqual(1, eliminado);
+        Assert.AreEqual(2, cola.VerPrimero());
+    }
+
+    [TestMethod]
+    public void TestEliminarUltimoElementoCircular()
+    {
+        var cola = new ColaCircular(3);
+        cola.Agregar(1);
+        cola.Agregar(2);
+        cola.Agregar(3);
+
+        cola.Eliminar();
+        cola.Eliminar();
+        int eliminado = cola.Eliminar();
+
+        Assert.AreEqual(3, eliminado);
+        Assert.AreEqual(-1, cola.VerPrimero());
+    }
+
+    [TestMethod]
+    public void TestDesbordamientoCircular()
+    {
+        var cola = new ColaCircular(2);
+        cola.Agregar(1);
+        cola.Agregar(2);
+
+        cola.Agregar(3);
+
+        Assert.AreEqual(1, cola.VerPrimero());
+    }
+
+    [TestMethod]
+    public void TestSubdesbordamientoCircular()
+    {
+        var cola = new ColaCircular(3);
+
+        int eliminado = cola.Eliminar();
+
+        Assert.AreEqual(-1, eliminado);
+    }
+
+    [TestMethod]
+    public void TestCircularidad()
+    {
+        var cola = new ColaCircular(3);
+        cola.Agregar(1);
+        cola.Agregar(2);
+        cola.Eliminar();
+        cola.Agregar(3);
+        cola.Agregar(4);
+
+        Assert.AreEqual(2, cola.VerPrimero());
+    }
+
+    [TestMethod]
+    public void TestAgregarConColaLlenaCircular()
+    {
+        var cola = new ColaCircular(3);
+        cola.Agregar(1);
+        cola.Agregar(2);
+        cola.Agregar(3);
+
+        cola.Agregar(4);
+
+        Assert.AreEqual(1, cola.VerPrimero());
+    }
+
+    [TestMethod]
+    public void TestVerPrimeroConColaVaciaCircular()
+    {
+        var cola = new ColaCircular(3);
+
+        int primero = cola.VerPrimero();
+
+        Assert.AreEqual(-1, primero);
+    }
+}
+
+
+
